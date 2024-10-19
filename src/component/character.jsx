556892fbde82ca62d5b8112d84d70baacec330cd
jihyeoniiiii <105184159+Jihyeoniiiii@users.noreply.github.com>
2024-10-19 17:38:25 +0900

    feat: add character icon

---
+++ src/component/character.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import RunIcon from '../assets/run-character.svg'
+
+const Character = () => {
+  return (
+    <img src={RunIcon} width={100} height={100} />
+  )
+}
+
+export default Character
