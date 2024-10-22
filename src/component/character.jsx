1c2b54c86b9afeb8337ce27b5f9bb401c8c5962b
jihyeoniiiii <105184159+Jihyeoniiiii@users.noreply.github.com>
2024-10-22 20:50:56 +0900

    feat: move character based on width prop

--- src/component/character.jsx
+++ src/component/character.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import RunIcon from '../assets/run-character.svg'
+import styled from 'styled-components'
 
-const Character = () => {
+const CharacterPosition = styled.div`
+  position: absolute;
+  top: -100px;
+  left: ${({ width }) => width}%; /* width에 따라 캐릭터 이동 */
+  transform: translateX(-50%); /* 캐릭터의 중앙을 기준으로 이동 */
+  transition: left 0.3s ease-in-out;
+`
+
+const Character = ({ width }) => {
   return (
-    <img src={RunIcon} width={100} height={100} />
+    <CharacterPosition width={width}>
+      <img src={RunIcon} width={100} height={100} />
+    </CharacterPosition>
   )
 }
 
