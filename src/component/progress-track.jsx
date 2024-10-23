897e77a76203394a4fcba9d6146dbc9906bd017d
jihyeoniiiii <105184159+Jihyeoniiiii@users.noreply.github.com>
2024-10-23 16:34:57 +0900

    feat: adjust progress bar width

--- src/component/progress-track.jsx
+++ src/component/progress-track.jsx
@@ -1,48 +1,73 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import Character from './character'
+import dayjs from 'dayjs'
+import PropTypes from 'prop-types'
 
 const Wrapper = styled.div`
+  width: 1400px;
   display: flex;
+  position: relative;
   flex-direction: column;
-  margin-top: 30px;
+  margin-top: 100px;
   margin-left: 180px;
 `
 
 const ProgressField = styled.div`
-    width: 1500px;
+    width: 100%;
     height: 30px;
     display: flex;
-    justify-content: center;
+    justify-content: start;
     align-items: center;
+    padding: 0 30px;
     background-color: white;
     border-radius: 20px;
-    margin: 10px;
     z-index: 10;
     position: relative;
     box-shadow: 0 2px 15px rgba(0, 0, 0, 0.1);
 `
 
+const ProgressNumber = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  margin-top: 80px;
+  color: gray;
+  font-weight: bold;
+`
+
 const ProgressBar = styled.div`
-    width: 95%;
+    width: ${({ width }) => (isNaN(width) ? 0 : width)}%;
     height: 10px;
     background-color: orange;
     border-radius: 20px;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
 `
 
-const ProgressTrack = () => {
+const ProgressTrack = ({ totalCheckedDays, lastDay }) => {
+  const [progressWidth, setProgressWidth] = useState(0);
+  
+  useEffect(() => {
+    const progressPercentage = (totalCheckedDays / lastDay) * 100;
+    setProgressWidth(progressPercentage);
+  }, [totalCheckedDays, lastDay]);
+
   return (
     <Wrapper>
-        <Character />
+        <Character width={progressWidth} />
         <ProgressField>
-            <ProgressBar />
+            <ProgressBar width={progressWidth} />
+            <ProgressNumber>
+              <span>{totalCheckedDays}</span>
+              <span>{lastDay}</span>
+            </ProgressNumber>
         </ProgressField>
     </Wrapper>
   )
 }
+
+ProgressTrack.propTypes = {
+  width: PropTypes.number,
+};
 
 export default ProgressTrack
